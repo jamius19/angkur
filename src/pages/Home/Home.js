852cfb21cf7ba9071633860917ff8bfb40196007
jamius19 <jamiussiam@gmail.com>
2019-05-21 16:26:00 +0600

    Added Spring.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Navbar from "../../components/Nav/Navbar";
 import styles from './Home.module.scss';
+import {Spring, config} from 'react-spring/renderprops';
 
 class Home extends Component {
    render() {
@@ -8,20 +9,25 @@ class Home extends Component {
           <div>
              <header className={"pt-4 " + styles.HeroBg}>
                 <Navbar/>
-                <div className="container">
-                   <div className="d-flex flex-column align-items-center text-center">
-                      <h3 className={"text-light " + styles.heroTitle}>
-                         আপনার ওয়েবসাইটে বাংলা ফন্ট অ্যাড করুন
-                         <br/>
-                         সহজে এবং বিনামূল্যে
-                      </h3>
-                      <div className={"d-flex flex-column flex-lg-row " + styles.coaButtons}>
-                         <button className={"cbtn cbtn-orange " + styles.cbtn}>ফন্টগুলো দেখুন</button>
-                         <button className="cbtn cbtn-outline-orange">আরো জানুন</button>
-                      </div>
-                   </div>
-                </div>
-
+                <Spring from={{transform: 'translate3d(0px, -20px, 0px)'}}
+                        to={{transform: 'translate3d(0px, 0px, 0px)'}}
+                        config={config.gentle}>
+                   {props => (
+                       <div className="container">
+                          <div className="d-flex flex-column align-items-center text-center">
+                             <h3 className={"text-light " + styles.heroTitle}>
+                                আপনার ওয়েবসাইটে বাংলা ফন্ট অ্যাড করুন
+                                <br/>
+                                সহজে এবং বিনামূল্যে
+                             </h3>
+                             <div className={"d-flex flex-column flex-lg-row " + styles.coaButtons}>
+                                <button className={"cbtn cbtn-orange " + styles.cbtn}>ফন্টগুলো দেখুন</button>
+                                <button className="cbtn cbtn-outline-orange">আরো জানুন</button>
+                             </div>
+                          </div>
+                       </div>
+                   )}
+                </Spring>
              </header>
           </div>
       );
