61c5cdec9fea53fa16842e60211c6cb977a20a4d
jamius19 <jamiussiam@gmail.com>
2019-05-21 11:59:16 +0600

    Removed Spring. Added polyfill.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Navbar from "../../components/Nav/Navbar";
 import styles from './Home.module.scss';
-import {config, Spring} from "react-spring/renderprops";
 
 class Home extends Component {
    render() {
@@ -9,30 +8,30 @@ class Home extends Component {
           <div>
              <header className={"pt-4 " + styles.HeroBg}>
                 <Navbar/>
+                <div className="container">
+                   <div className="d-flex flex-column align-items-center text-center">
+                      <h3 className={"text-light " + styles.heroTitle}>
+                         আপনার ওয়েবসাইটে বাংলা ফন্ট অ্যাড করুন
+                         <br/>
+                         সহজে এবং বিনামূল্যে
+                      </h3>
+                      <div className={"d-flex flex-column flex-lg-row " + styles.coaButtons}>
+                         <button className={"cbtn cbtn-orange " + styles.cbtn}>ফন্টগুলো দেখুন</button>
+                         <button className="cbtn cbtn-outline-orange">আরো জানুন</button>
+                      </div>
+                   </div>
+                </div>
 
-                <Spring from={{opacity: 0, transform: 'translate3d(0px, -20px, 0px)'}}
-                        to={{opacity: 1, transform: 'translate3d(0px, 0px, 0px)'}}
-                        config={config.gentle}>
-                   {props => (
-                       <div style={props} className="container">
-                          <div className="d-flex flex-column align-items-center text-center">
-                             <h3 className={"text-light " + styles.heroTitle}>
-                                আপনার ওয়েবসাইটে বাংলা ফন্ট অ্যাড করুন
-                                <br/>
-                                সহজে এবং বিনামূল্যে
-                             </h3>
-                             <div className={"d-flex flex-column flex-lg-row " + styles.coaButtons}>
-                                <button className={"cbtn cbtn-orange " + styles.cbtn}>ফন্টগুলো দেখুন</button>
-                                <button className="cbtn cbtn-outline-orange">আরো জানুন</button>
-                             </div>
-                          </div>
-                       </div>
-                   )}
-                </Spring>
              </header>
           </div>
       );
    }
 }
+
+/*<Spring from={{opacity: 0, transform: 'translate3d(0px, -20px, 0px)'}}
+                        to={{opacity: 1, transform: 'translate3d(0px, 0px, 0px)'}}
+                        config={config.gentle}>
+                   {props => ()}
+                </Spring>*/
 
 export default Home;
